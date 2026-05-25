3c9d227ed981bb6ac7468a31f858053d3857e413
MonicaPar <20002603@galileo.edu>
2026-05-25 00:06:33 -0600

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import pool from './db'
 import serviciosRouter from './routes/servicios'
 import productosRouter from './routes/productos'
 import confirmacionesRouter from './routes/confirmaciones'
