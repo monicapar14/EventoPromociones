cfe2a315eb75e265b5a736324123c1cb0db0a1ad
MonicaPar <20002603@galileo.edu>
2026-05-25 00:31:37 -0600

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,32 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import serviciosRouter from './routes/servicios'
+import productosRouter from './routes/productos'
+import confirmacionesRouter from './routes/confirmaciones'
+
+dotenv.config()
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 const PORT = Number(process.env.PORT) || 8080
 
 app.get('/', (req, res) => {
-  res.send('Backend funcionando correctamente')
+  res.send('Backend funcionando')
 })
 
-app.listen(PORT, '0.0.0.0', () => {
+// Rutas de servicios
+app.use('/api/servicios', serviciosRouter)
+
+//Ruta de productos
+app.use('/api/productos', productosRouter)
+
+//Ruta de confirmaciones
+app.use('/api/confirmaciones', confirmacionesRouter)
+
+app.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Servidor corriendo en puerto ${PORT}`)
 })
